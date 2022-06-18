ae7f847fdf8b371e7b058464f8668436b380c190
Pierce <ppuckette@gmail.com>
2022-06-17 21:46:49 -0400

    accordion complete

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,8 +1,27 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+	const [questions, setQuestions] = useState(data);
+
+	return (
+		<main>
+			{/* create container for each question */}
+			<div className="container">
+				<h3>questions and answers about login</h3>
+				<section className="info">
+					{/* traverse the array and extract the data per id */}
+					{questions.map((question) => {
+						return (
+							<SingleQuestion key={question.id} {...question}>
+								{" "}
+							</SingleQuestion>
+						);
+					})}
+				</section>
+			</div>
+		</main>
+	);
 }
 
 export default App;
--- setup/src/Question.js
+++ setup/src/Question.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  
+	return (
+		<article className="question">
+			<header>
+				<h4>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)} >
+          {/* check to see if the icon is a plus or minus then change it */}
+          {/* upon clicking on the icon toggle the info on or off */}
+          { showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {/* show answer if showInfo is true  */}
+			{showInfo && <p>{info}</p>}
+		</article>
+	);
 };
 
 export default Question;
